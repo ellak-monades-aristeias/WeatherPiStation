9eeb0e76362276d7874e0cf9734cca3fad2fc8df
mbalamat <mbalamatsias@gmail.com>
2015-10-09 16:12:36 +0300

    Debug1

--- www/push.php
+++ www/push.php
@@ -6,7 +6,21 @@ try {
     $dbh = new PDO('mysql:dbname='.$dbname.';host='.$servername.';port='.$port, $username, $password);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    echo 'success connecting to DB!';
+    //echo 'success connecting to DB!';
+    try {
+        if (isset($_GET["APIkey"]) && isset($_GET["data"])) {
+            //TODO Check APIkey for validity and security
+           $apiKey = $_GET["APIkey"];
+           $data = $_GET["data"];
+
+           $parsed = json_decode($data, true);
+           echo "debug\n";
+           echo $parsed[Date and time];
+
+        }
+    } catch {
+        echo '{"Error" :  "Reading APIkey or data. Please check if data is in json form!"}';
+    }
 
     $dbh = null;
 } catch (PDOException $e) {
