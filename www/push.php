b6fd7a60bdf55fa1bd9f54328649165be8ca3c5c
mbalamat <mbalamatsias@gmail.com>
2015-10-12 17:45:10 +0300

    modified:   push.php

--- www/push.php
+++ www/push.php
@@ -6,7 +6,7 @@ try {
     $dbh = new PDO('mysql:dbname='.$dbname.';host='.$servername.';port='.$port, $username, $password);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo 'success connecting to DB!';
-    if (isset($_GET["APIkey"]) && isset($_POST["data"])) {
+    if (isset($_GET["APIkey"]) && isset($_GET["data"])) {
         //TODO Check APIkey for validity and security
         $apiKey = $_GET["APIkey"];
         $data = $_GET["data"];
