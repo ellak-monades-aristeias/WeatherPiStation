8743e018a3cbadf0368b50642e021540bd32db5e
mbalamat <mbalamatsias@gmail.com>
2015-10-12 17:51:14 +0300

    Add push.php final

--- www/push.php
+++ www/push.php
@@ -5,17 +5,14 @@ try {
 
     $dbh = new PDO('mysql:dbname='.$dbname.';host='.$servername.';port='.$port, $username, $password);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo 'success connecting to DB!';
     if (isset($_GET["APIkey"]) && isset($_GET["data"])) {
         //TODO Check APIkey for validity and security
         $apiKey = $_GET["APIkey"];
         $data = $_GET["data"];
-
         $stmt = $dbh->prepare("INSERT INTO `weatherDB` (`RPi_id`, `jsonData`) VALUES (:id, :jsonData)");
         $stmt->bindParam(':id', $apiKey);
         $stmt->bindParam(':jsonData', $data);
         $stmt->execute();
-        echo 'Debug1';
     }
     $dbh = null;
 } catch (PDOException $e) {
