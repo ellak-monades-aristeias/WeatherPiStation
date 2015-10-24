d24a6313b8055d3a453346bd33555248987fe795
mbalamat <mbalamatsias@gmail.com>
2015-10-24 16:28:23 +0300

    Change repo structure

---
+++ www/front/charts.js
@@ -0,0 +1,13 @@
+function makeChart() {
+    /*var urls = "https://weatherpistation-mbalamat.rhcloud.com/API/get.php?rpiID=".concat(document.getElementById("sel1").value);
+    $.ajax({
+        url: urls,
+        dataType: 'json',
+        type: 'get',
+        cache: false,
+        success: function(data){
+           $(data.Temperature-BMP).each(function(index, value) {
+            console.log(value);
+           }); 
+        }*/
+}
