7defc3e64a2f160eb383d8f7248868834291c72f
mbalamat <mbalamatsias@gmail.com>
2015-10-27 14:36:09 +0200

    Update repo

--- www/front-app/charts.js
+++ www/front-app/charts.js
@@ -1,13 +1,10 @@
 function makeChart() {
-    /*var urls = "https://weatherpistation-mbalamat.rhcloud.com/API/get.php?rpiID=".concat(document.getElementById("sel1").value);
-    $.ajax({
-        url: urls,
-        dataType: 'json',
-        type: 'get',
-        cache: false,
-        success: function(data){
-           $(data.Temperature-BMP).each(function(index, value) {
-            console.log(value);
-           }); 
+    var urls = "https://weatherpistation-mbalamat.rhcloud.com/API/get.php?rpiID=".concat(document.getElementById("sel1").value);
+    $.getJSON(urls, function(data) { 
+        /*for(var propertyName in data) {
+            console.log(propertyName); 
         }*/
+        console.log(data.data[0].Pressure);
+    });
+
 }
