67e53219b746c17ede1169a82a8cc393e5a19399
mbalamat <mbalamatsias@gmail.com>
2015-10-30 03:51:34 +0200

    Add colors to charts

--- www/front-app/charts.js
+++ www/front-app/charts.js
@@ -3,27 +3,41 @@ function makeChart() {
     $.getJSON(urls, function(data) { 
         var xAxis = [];
         var yAxis = [];
+        var fill_color;
+        var stroke_color;
+        var point_color;
         for (var i=0; i < data.data.length; i++){
             xAxis.push(data.data[i]['Date and time']);
             if (document.getElementById("sel2").value === 'Θερμοκρασία'){
                 yAxis.push(data.data[i]['Temperature-DHT']);
+                fill_color = "rgba(255,10,43,0.2)";
+                stroke_color = "rgba(255,10,43,1)";
+                point_color = "rgba(255,10,43,1)";
             }
             if (document.getElementById("sel2").value === 'Υγρασία'){
                 yAxis.push(data.data[i]['Relative_Humidity']);
+                fill_color = "rgba(0,255,255,0.2)";
+                stroke_color = "rgba(0,255,255,1)";
+                point_color = "rgba(0,255,255,1)";
             }
             if (document.getElementById("sel2").value === 'Πίεση'){
                 yAxis.push(data.data[i]['Pressure']);
+                fill_color = "rgba(75,75,75,0.2)";
+                stroke_color = "rgba(75,75,75,1)";
+                point_color = "rgba(75,75,75,1)";
             }
         }
+        yAxis = yAxis.reverse();
+        xAxis = xAxis.reverse();
         var ctx = document.getElementById("myChart").getContext("2d");
         var data = {
             labels: xAxis,
             datasets: [
                 {
-                    label: "mychart",
-                    fillColor: "rgba(220,220,220,0.2)",
-                    strokeColor: "rgba(220,220,220,1)",
-                    pointColor: "rgba(220,220,220,1)",
+                    label: document.getElementById("sel2").value,
+                    fillColor: fill_color,
+                    strokeColor: stroke_color, 
+                    pointColor: point_color,
                     pointStrokeColor: "#fff",
                     pointHighlightFill: "#fff",
                     pointHighlightStroke: "rgba(220,220,220,1)",
@@ -48,5 +62,8 @@ function makeChart() {
             legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
         };
         var myLineChart = new Chart(ctx).Line(data, options);
+        $('html, body').animate({
+            scrollTop: $("#myChart").offset().top
+            }, 1000);
     });
 }
